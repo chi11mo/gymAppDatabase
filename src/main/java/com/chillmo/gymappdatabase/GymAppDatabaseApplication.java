b8a7b710ae0169972005440469692eca14e47c10
Dominic <dominic.wenzel@mailbox.org>
2024-02-23 01:11:24 +0100

    init commit

---
+++ src/main/java/com/chillmo/gymappdatabase/GymAppDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.chillmo.gymappdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAppDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymAppDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chillmo/gymappdatabase/GymAppDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chillmo.gymappdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymAppDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
